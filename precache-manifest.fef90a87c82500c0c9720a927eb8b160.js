dcfa257faf2a4c0dce78e6a1f1a6ede378e49686
Tauhid Hasan <tauhidhasan11@gmail.com>
2020-07-03 01:23:47 +0300

    Updates

---
+++ precache-manifest.fef90a87c82500c0c9720a927eb8b160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1ede2f1091cfb5de9ba11340a86410",
+    "url": "/tauhidhasan/index.html"
+  },
+  {
+    "revision": "8511ecbc109fe0ed392a",
+    "url": "/tauhidhasan/static/css/main.19d831d6.chunk.css"
+  },
+  {
+    "revision": "8ca7e712f1617548d6a7",
+    "url": "/tauhidhasan/static/js/2.1dedde08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tauhidhasan/static/js/2.1dedde08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8511ecbc109fe0ed392a",
+    "url": "/tauhidhasan/static/js/main.f4e8ea86.chunk.js"
+  },
+  {
+    "revision": "64c23ba507cc1e880b74",
+    "url": "/tauhidhasan/static/js/runtime-main.8ef957e7.js"
+  }
+]);
